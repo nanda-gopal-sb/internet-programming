cd1ab57f1c6237df95cd3dd3d184444babf7d5b4
Nandagopal <nandagopal.nsb@gmail.com>
2024-12-09 14:43:58 +0530

    Added file

--- js/dom.js
+++ js/dom.js
@@ -1,12 +1,22 @@
+
+const get = confirm("WIll you be my friend?");
+
+
+
 const button = document.getElementById("button");
 const text = document.getElementById("underlined");
 
 const paragraphs = document.querySelectorAll("p");
 const formHandeler = document.getElementById("formLAMO");
 
+
+const input = formHandeler.querySelectorAll("input");
+
 formHandeler.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("form submitted");
+  console.log(input[0].value);
+  console.log(input[1].value);
 });
 button.addEventListener("click", () => {
   text.innerHTML = "underline";
