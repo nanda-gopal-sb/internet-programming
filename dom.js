4cfa1370347c52bbd4945860638985892e8ac8b8
Nandagopal <nandagopal.nsb@gmail.com>
2024-12-09 13:40:23 +0530

    Added js

---
+++ dom.js
@@ -0,0 +1,4 @@
+// The DOM is the central programming API for the JS language to traverse the HTML tree. 
+// The DOM represents the document as nodes and objects; that way, programming languages can interact with the page. 
+//The DOM is not part of the JavaScript language, but is instead a Web API used to build websites.
+//
--- js/dom.js
+++
@@ -1,2 +0,0 @@
-const body = document.body;
-const div = document.createElement('div'); 
